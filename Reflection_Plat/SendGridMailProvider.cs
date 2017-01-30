c9cb31baa2439ac1006c58369460d3237a1575c6
Ilker Halil Türer <iturer@hurriyetemlak.com>
2017-01-30 15:39:04 +0300

    add todo

---
+++ Reflection_Plat/SendGridMailProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reflection_Plat
+{
+    public class SendGridMailProvider : IMailProvider
+    {
+        public void SendEmail(string @from, string to, string cc, string bcc, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Reflection_Plat/SmtpMailProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Reflection_Plat
+{
+    public class SmtpMailProvider : IMailProvider
+    {
+        //todo:smtp protokulune göre mail atacak sınıf implement edilecek..!
+        public void SendEmail(string @from, string to, string cc, string bcc, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

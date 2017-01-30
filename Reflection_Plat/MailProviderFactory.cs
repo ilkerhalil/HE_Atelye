eb7c28dd072791be482370da8f988e7db2906880
Ilker Halil Türer <iturer@hurriyetemlak.com>
2017-01-30 15:34:48 +0300

    first commit

---
+++ Reflection_Plat/MailProviderFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection_Plat {
+    public static class MailProviderFactory {
+        public static IMailProvider CreateMailProvider(ProviderEnum providerEnum)
+        {
+            switch (providerEnum)
+            {
+                case ProviderEnum.Smpt:
+                    return new SmtpMailProvider();
+                case ProviderEnum.EuroMessage:
+                    return new EuroMessageMailProvider();
+                case ProviderEnum.SendGrid:
+                    return new SendGridMailProvider();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(providerEnum), providerEnum, null);
+            }
+        }
+    }
+
+    public  enum ProviderEnum
+    {
+        Smpt=1,EuroMessage=2,SendGrid=3
+    }
+}
---
+++ Reflection_Plat/ProviderEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace Reflection_Plat
+{
+    public  enum ProviderEnum
+    {
+        Smpt=1,EuroMessage=2,SendGrid=3
+    }
+}
